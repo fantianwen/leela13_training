033ebc52e20da0ee0fedeb6552799bf572d6ba98
RadAsm <twfan_09@hotmail.com>
2019-04-04 21:44:59 +0900

    first

---
+++ autoTrain/minitrain.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+suffix=$1
+gpunum=$2
+
+for((i=0;i<50000;i=i+1)) ; do
+    resign_rate=$((i%10))
+    timestamp=$(date +%y%m%d_%H%M%S)_${suffix}
+    latest_weight=$(ls -1c *.txt | head -1)
+    leelaz_cmd="../build/leelaz -m 8 -n -d -r $resign_rate -t 1 -p 1000 --noponder --gtp --gpu $gpunum"
+    sleep 5
+    echo leelaz_cmd : $leelaz_cmd
+    echo latest_weight : $latest_weight
+    echo timestamp : $timestamp
+    echo autotrain traindata_${timestamp} 25 \n quit | ${leelaz_cmd} -w $latest_weight
+done
